0c7a7f9a006ff4caca313d31d838b421058031f5
daniel.bradea <daniel.bradea@ferendin.com>
2025-03-15 21:18:36 +0200

    update id user dto

--- src/main/java/com/bid90/psm/client/dto/UserDto.java
+++ src/main/java/com/bid90/psm/client/dto/UserDto.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    Long id;
+    UUID id;
     String firstName;
     String lastName;
     String email;
